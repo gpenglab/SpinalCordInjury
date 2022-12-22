d2265b78803a9ff5e7e0a2e6c02fee69ad6491f6
gpenglab <98580084+gpenglab@users.noreply.github.com>
2022-12-22 20:57:07 +0800

    Delete script directory

--- script/self_function/save_pheatmap_pdf.R
+++
@@ -1,8 +0,0 @@
-save_pheatmap_pdf <- function(x, filename, width=7, height=7) {
-   stopifnot(!missing(x))
-   stopifnot(!missing(filename))
-   pdf(filename, width=width, height=height)
-   grid::grid.newpage()
-   grid::grid.draw(x$gtable)
-   dev.off()
-}
